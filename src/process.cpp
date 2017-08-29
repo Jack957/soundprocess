39c5537ad5050eed17d7947211fe7e3c1f26caf5
George Oblapenko <kunstmord@kunstmord.com>
2017-08-29 18:49:27 +0300

    added write-audio-to-file function

--- src/process.cpp
+++ src/process.cpp
@@ -5,27 +5,58 @@
 #include <vector>
 #include "AudioFile.h"
 
-std::vector<arma::vec> load_audio(const std::string &filename) {
+AudioFile<double> load_audio(const std::string &filename) {
     std::ifstream ifs(filename);
     std::string wavfilename;
     std::getline(ifs, wavfilename);
 
-    AudioFile<double> audioFile;
-    audioFile.load(wavfilename);
-    int numChannels = audioFile.getNumChannels();
-    int numSamples = audioFile.getNumSamplesPerChannel();
+    AudioFile<double> audio_file;
+    audio_file.load(wavfilename);
+    return audioFile;
+}
+
+std::vector<arma::vec> convert_audio(const AudioFile<double> &audio_file) {
+    int num_channels = audio_file.getNumChannels();
+    int num_samples = audio_file.getNumSamplesPerChannel();
 
     std::vector<arma::vec> output;
     arma::vec channel = arma::zeros(numSamples);
-    int i, k;
 
-    for (i=0; i<numChannels; i++) {
-        for (k=0; k<numSamples; k++) {
+    for (int i=0; i<num_channels; i++) {
+        for (int k=0; k<num_samples; k++) {
             channel[k] = audioFile.samples[i][k];
         }
         output.push_back(channel);
     }
     return output;
+}
+
+AudioFile<double> convert_audio(const std::vector<arma::vec> &audio, int sample_rate=44100, int bit_depth=16) {
+    int num_channels = audio.size();
+    AudioFile<double>::AudioBuffer buffer;
+    buffer.resize(num_channels);
+    int i, k;
+    for (i=0; i<num_channels; i++) {
+        buffer[i].resize(audio[i].n_elem);
+        for (int k=0; k<audio[i].n_elem; k++) {
+            buffer[i][k] = audio[i][k];
+        }
+    }
+
+    AudioFile<double> audio_file;
+    bool ok = audio_file.setAudioBuffer(buffer);
+    audio_file.setBitDepth(bit_depth);
+    audio_file.setSampleRate(sample_rate);
+    return audio_file;
+}
+
+void write_audio(const std::vector<arma::vec> &audio, const std::string &filename, int sample_rate=44100, int bit_depth=16) {
+    AudioFile<double> audio_file = convert_audio(audio, sample_rate, bit_depth);
+    write_audio(audio_file, filename);
+}
+
+void write_audio(const AudioFile<double> &audio_file, const std::string &filename) {
+    audio_file.save(filename);
 }
 
 int main(int argc, char const *argv[])
