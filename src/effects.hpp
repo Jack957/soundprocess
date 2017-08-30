f014020262ce466411bba25852e319aa87531bc9
George Oblapenko <kunstmord@kunstmord.com>
2017-08-30 15:48:53 +0300

    single delay

--- src/effects.hpp
+++ src/effects.hpp
@@ -1,7 +1,7 @@
 #ifndef SP_EFFECTS_HPP
 #define SP_EFFECTS_HPP
 
-void single_delay(const std::vector<arma::vec> &audio, double delay_time, int sample_rate, std::vector<arma::vec> &audio_output);
-std::vector<arma::vec> single_delay(const std::vector<arma::vec> &audio, double delay_time, int sample_rate, bool auto_pad=false);
+void single_delay(const std::vector<arma::vec> &audio, double delay_time, double wet_mix, int sample_rate, std::vector<arma::vec> &audio_output);
+std::vector<arma::vec> single_delay(const std::vector<arma::vec> &audio, double delay_time, double wet_mix, int sample_rate, bool auto_pad=false);
 
 #endif
