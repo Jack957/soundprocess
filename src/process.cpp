977e2411e0f0e188e182f30eaee61b7d3be8010e
George Oblapenko <kunstmord@kunstmord.com>
2017-09-08 19:24:52 +0300

    some fixes in file loading

--- src/process.cpp
+++ src/process.cpp
@@ -2,17 +2,27 @@
 #include "utils.hpp"
 #include "effects.hpp"
 
-int main(int argc, char const *argv[])
+int main(int argc, char const *argv[]) // effect type, input, output, 
 {
-    std::string filename;
+    std::string in_filename = "input.wav", out_filename = "output.wav";
     if (argc==1) {
-        filename = "input";
+        return 0;
+    }
+    else if (argc==2) {
+        in_filename = "input.wav";
+        out_filename = "output.wav";
+    }
+    else if (argc==3) {
+        in_filename = argv[3];
     }
     else {
-        filename = argv[1];
+        in_filename = argv[3];
+        out_filename = argv[4];
     }
 
-    std::vector<arma::vec> audiodata = load_audio(filename);
-    std::cout << audiodata[0].n_elem << std::endl;
+    AudioFile<double> audiodata = load_audio(filename);
+    std::vector<arma::vec> arma_audiodata = convert_audio(audiodata);
+    // arma_audiodata = process(arma_audiodata);
+    // write_audio(arma_audiodata, out_filename, int sample_rate, int bit_depth) 
     return 0;
 }
